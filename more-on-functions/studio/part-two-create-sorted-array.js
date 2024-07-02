92a3787e3353eff1c0c1be3aa320585ab7d80c69
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-07-01 21:30:06 -0400

    Studio exercise

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -1,7 +1,7 @@
-function findMinValue(arr){
+function findMinValue(arr) {
   let min = arr[0];
-  for (i = 0; i < arr.length; i++){
-    if (arr[i] < min){
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
       min = arr[i];
     }
   }
@@ -11,6 +11,7 @@ function findMinValue(arr){
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 
 /*Within the function:
+
 1) Define a new, empty array to hold the final sorted numbers.
 2) Use the findMinValue function to find the minimum value in the old array.
 3) Add the minimum value to the new array, and remove the minimum value from the old array.
@@ -19,6 +20,32 @@ function findMinValue(arr){
 6) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
+
+function sortArray(numArray) {
+  let sortedArray = [];
+  counter = numArray.length;
+  for (let i = 0; i < counter; i++) {
+    if (numArray.length > 1) {
+      console.log(`iteration: ${i + 1}`);
+      console.log(`Num Array: ${numArray}`);
+      let minValue = findMinValue(numArray);
+      console.log(`min value: ${minValue}`);
+      sortedArray.push(minValue);
+
+      console.log(`Splicing: ${numArray.indexOf(minValue)}`);
+      numArray.splice(numArray.indexOf(minValue), 1);
+
+      console.log(`Num Array: ${numArray}`);
+    } else {
+      console.log(`min value: ${numArray[0]}`);
+      sortedArray.push(numArray[0]);
+
+      console.log(`Splicing: ${numArray[0]}`);
+      numArray.splice(0, 1);
+    }
+  }
+  return sortedArray;
+}
 
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
