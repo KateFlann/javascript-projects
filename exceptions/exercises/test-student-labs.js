5d7c6adaa9a713f0a6174d981dad360cf1b4a713
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-07-17 22:36:06 -0400

    Exceptions Exercise

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,34 @@
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
+  for (let i = 0; i < labs.length; i++) {
     let lab = labs[i];
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
 
-let studentLabs = [
+let studentLabs2 = [
   {
-    student: 'Carly',
-    runLab: function (num) {
-        return Math.pow(num, num);
-    }
+    student: "Blake",
+    myCode: function (num) {
+      return Math.pow(num, num);
+    },
   },
   {
-    student: 'Erica',
+    student: "Jessica",
     runLab: function (num) {
-        return num * num;
-    }
-  }
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: "Mya",
+    runLab: function (num) {
+      return num * num;
+    },
+  },
 ];
 
-gradeLabs(studentLabs);
+try {
+  gradeLabs(studentLabs2);
+} catch (error) {
+  throw Error("Error thrown");
+}
