af944291ad167fc74cf7f065b6f7ee20a32ae947
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-07-24 10:41:11 -0400

    Unit Testing Studio commit

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,61 @@
-
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: "100%",
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function (num) {
+    let message = {
+      1: "Launch",
+      2: "Code",
+      3: "Rocks",
+    };
+
+    let LaunchCodeString = "";
+    for (property in message) {
+      if (num % property === 0) {
+        LaunchCodeString += message[property];
+      }
+    }
+    if (LaunchCodeString === "") {
+      return "Rutabegas! That doesn't work.";
+    } else {
+      if (num % 2 === 0 && num % 5 === 0 && num % 3 !== 0) {
+        return (LaunchCodeString += "! (Crash!!!!)");
+      }
+      return LaunchCodeString.trim() + "!";
+    }
+    // if (num % 2 === 0) {
+    //   if (num % 3 === 0) {
+    //     if (num % 5 === 0) {
+    //       return "LaunchCode Rocks!";
+    //     }
+    //     return "LaunchCode!";
+    //   }
+    //   if (num % 5 === 0) {
+    //     return "Launch Rocks! (CRASH!!!!)";
+    //   }
+    //   return "Launch!"
+    // }
+    // if (num % 3 === 0) {
+    //   if (num %5 === 0) {
+    //     return "Code Rocks!";
+    //   }
+    //   return "Code!"
+    // }
+    // if (num % 5 === 0) {
+    //   return "Rocks!"
+    // }
+  },
+};
+
+// launchOutput() needs to meet the following conditions:
+// When passed a number that is ONLY divisible by 2, return 'Launch!'
+// When passed a number that is ONLY divisible by 3, return 'Code!'
+// When passed a number that is ONLY divisible by 5, return 'Rocks!'
+// When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'
+// When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'
+// When passed a number that is divisible by 2 AND 5, return 'Launch Rocks!'
+// When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'
+// When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,46 @@
-// launchcode.test.js code:
-const launchcode = require('../index.js');
+const launchcode = require("../index.js");
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode"),
+  function () {
+    //Write your unit tests here!
 
-  // Write your unit tests here!
-  
-});
+    test("check organization type", function () {
+      expect(launchcode.organization).toEqual("nonprofit");
+    });
+
+    test("check executive director name", function () {
+      expect(launchcode.ExecutiveDirector).toEqual("Jeff");
+    });
+
+    test("check cool employees", function () {
+      expect(launchcode.percentageCoolEmployees).toEqual("100%");
+    });
+
+    test("returns an array of values 'Web Development', 'Data Analysis', 'Liftoff'.", function () {
+      let output = launchcode.programsOffered;
+      expect(output).toContain("Web Development");
+      expect(output).toContain("Data Analysis");
+      expect(output).toContain("Liftoff");
+      expect(output.length).toEqual(3);
+    });
+
+    test("should return 'Launch!' when LaunchOutput is passed a number that is ONLY divisible by 2.", () => {
+      let output = launchcode.launchOutput(2);
+      expect(output).toEqual("Launch!");
+    });
+
+    test("should return 'Launch!' when LaunchOutput is passed a number that is ONLY divisible by 3.", () => {
+      let output = launchcode.launchOutput(3);
+      expect(output).toEqual("Code!");
+    });
+
+    test("should return 'Launch!' when LaunchOutput is passed a number that is ONLY divisible by 5.", () => {
+      let output = launchcode.launchOutput(5);
+      expect(output).toEqual("Rocks!");
+    });
+
+    test("should return 'Launch!' when LaunchOutput is passed a number that is ONLY divisible by 2 and 3.", () => {
+      let output = launchcode.launchOutput(6);
+      expect(output).toEqual("Launchcode!");
+    });
+  };
