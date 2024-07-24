cfe9b0b6801e84be787c2a387a0d14d5b84e5996
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-07-24 11:28:13 -0400

    Class Studio final commit

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -3,10 +3,35 @@ class CrewCandidate {
   constructor(name, mass, scores) {
     this.name = name;
     this.mass = mass;
-    this.score = scores;
+    this.scores = scores;
   }
   addScore(newScore) {
-    this.score.push(newScore);
+    this.scores.push(newScore);
+  }
+
+  average() {
+    let sum = 0;
+    for (let i = 0; i < this.scores.length; i++) {
+      sum += this.scores[i];
+    }
+    return sum / this.scores.length;
+  }
+
+  status() {
+    let status = "";
+    if (this.average() >= 90) {
+      status = "Accepted";
+    } else if (this.average() >= 80 && this.average() <= 89) {
+      status = "Reserve";
+    } else if (this.average() >= 70 && this.average() <= 79) {
+      status = "Probationary";
+    } else if (this.average() <= 69) {
+      status = "Rejected";
+    }
+
+    return `${
+      this.name
+    } earned an average test score of ${this.average()}% and has a status of ${status}.`;
   }
 }
 
@@ -14,13 +39,20 @@ let bear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
 let dog = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
 let gator = new CrewCandidate("Glad Gator", 225, [72, 78, 62]);
 
-console.log(bear.CrewCandidate);
-console.log(dog.CrewCandidate);
-console.log(gator.CrewCandidate);
+console.log(bear);
+console.log(dog);
+console.log(gator);
 
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
 
+//should log all scores plus 83
 bear.addScore(83);
-console.log(bear.CrewCandidate);
+console.log(bear);
+
+//average should be 92.7.
+console.log(dog.average());
 
 //Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
+console.log(bear.status());
+console.log(dog.status());
+console.log(gator.status());
