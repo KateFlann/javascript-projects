31c1234c2840446a1b1b974a66e93cd54ff44b54
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-08-05 18:04:47 -0400

    dom exercise 1

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,28 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+  // Put your code for the exercises here.
+
+  button.addEventListener("click", function (event) {
+    paragraph.innerHTML = "Houston! We have liftoff!";
+  });
+
+  missionAbort.addEventListener("mouseover", function (event) {
+    missionAbort.style.background = "red";
+  });
+
+  missionAbort.addEventListener("mouseout", function (event) {
+    missionAbort.style.background = "";
+  });
+
+  missionAbort.addEventListener("click", function (event) {
+    let answer = window.confirm("Are you sure you want to abort the mission?");
+    if (answer === true) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+    }
+  });
 }
 
 window.addEventListener("load", init);
